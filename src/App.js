e8057b21a3f35a46eec7d05979307e7025f86c05
drzackyll <adams.z.d@gmail.com>
2017-05-25 15:45:01 -0400

    Updates background image, nav bar

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ const App = () => {
 
   return (
     <div>
-      <div className="container-fluid" style={{backgroundColor: "rgb(35, 1, 1)"}}>
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-sm-7">
+          <div className="col-sm-6">
             <h1>BoatrideNYC</h1>
           </div>
-          <div className="col-sm-5">
+          <div className="col-sm-6">
             <h4 style={{color: "rgb(83, 100, 17)"}}>
               Call or email Captain John for rates and availability <br />
               john@boatridenyc.com | 914-772-7335
@@ -25,14 +25,14 @@ const App = () => {
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-7">
+          <div className="col-sm-6">
             <h4>Harbor Tours and Charters</h4>
           </div>
-          <div onClick={toSection} className="col-sm-1">
-            <a href="#photos">Photos</a>
+          <div onClick={toSection} className="col-sm-1 col-sm-offset-2">
+            <h4><a href="#photos">Photos</a></h4>
           </div>
-          <div onClick={toSection} className="col-sm-4">
-            <a href="#about">About</a>
+          <div onClick={toSection} className="col-sm-1">
+            <h4><a href="#about">About</a></h4>
           </div>
         </div>
       </div>
@@ -41,15 +41,15 @@ const App = () => {
         <img src={require("../public/images/background-2.jpg")} style={{width: "100%"}} alt="invisible" />
       </div>
 
-      <div className="container-fluid" id="photos">
-        <h2 style={{borderBottom: "solid"}}>Photos</h2>
-        <Photos />
-      </div>
-
       <div className="container-fluid" id="about">
         <h2 style={{borderBottom: "solid"}}>About</h2>
         Captain John is a maestro of New York Harbor and the surrounding rivers,
         inlets, and waterfronts, as well as broader NYC geography and history.
+      </div>
+
+      <div className="container-fluid" id="photos">
+      <h2 style={{borderBottom: "solid"}}>Photos</h2>
+      <Photos />
       </div>
     </div>
   );
